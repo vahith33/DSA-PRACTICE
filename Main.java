08dc51c7b2cda868b44fafbc7eef22bcd83a2f2a
vahith33 <162539759+vahith33@users.noreply.github.com>
2025-02-14 11:05:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        //creation
+
+        HashMap<String,String> map = new HashMap<>();
+
+        //insterstion
+
+        map.put("TN","Tamilnadu");
+        map.put("KA","Karnataka");
+        map.put("KL","Kerala");
+
+        //get
+
+       System.out.println(map.get("TN"));
+       System.out.println(map.get("KL"));
+       System.out.println(map.getOrDefault("AP","Not Found"));
+       System.out.println(map.values());
+       System.out.println(map.keySet());
+
+       //traversal
+
+       for(String key:map.keySet()){
+           System.out.println(key +" "+ map.get(key));
+       }
+
+       //remove
+
+        System.out.println(map.containsKey("AP"));
+        map.remove("KL");
+
+        //size
+        System.out.println(map.size());
+
+        //empty
+        System.out.println(map.isEmpty());
+    }
+}
